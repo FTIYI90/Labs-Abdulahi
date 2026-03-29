d689806cc584f1532deeae5ef62392a131c43467
Abdulahi Hassen <abdala.baaji@gmail.com>
2026-03-29 15:15:56 +0300

    Lab 8

--- Lab Practice/Lab 8 Next.js Web API/Tutorial/B54/myfinance-api/app/api/accounts/route.js
+++ Lab Practice/Lab 8 Next.js Web API/Tutorial/B54/myfinance-api/app/api/accounts/route.js
@@ -18,9 +18,21 @@ async function writeAccounts(data) {
 }
 
 
-export async function GET() {
+export async function GET(request) {
+
+    // type = savings & status=active
+    const { searchParams } = new URL(request.url);
+    const type = searchParams.get("type");
+    const status = searchParams.get("status");
+
     const accounts = await readAccounts()
-    return NextResponse.json(accounts, { status: 200 })
+
+    if (!type && !status) {
+        return NextResponse.json(accounts, { status: 200 })
+    }
+
+    const filteredAccounts = accounts.filter(a => a.type == type && a.status == status)
+    return NextResponse.json(filteredAccounts, { status: 200 })
 }
 
 export async function POST(request) {
