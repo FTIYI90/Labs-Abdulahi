899548ec9a36996e1c465f16da179f48233f3e64
Abdulahi Hassen <abdala.baaji@gmail.com>
2026-04-13 14:31:48 +0300

    updated

--- Lab Practice/Lab 9 Next.js React Pages and Components/Tutorial/B53/my-finance-app/app/transactions/page.jsx
+++ Lab Practice/Lab 9 Next.js React Pages and Components/Tutorial/B53/my-finance-app/app/transactions/page.jsx
@@ -1,9 +1,25 @@
 // rfce
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 
 function Transactions() {
+
+    const [counter, setCounter] = useState(0);
+
+    function incrementCounter() {
+        setCounter(counter + 1);
+        console.log("Incrementing Counter", counter);
+    }
+
+    useEffect(() => {
+        console.log("counter updated")
+    }, [counter])
+
     return (
-        <div><h1>Transactions Page Is Loaded</h1></div>
+        <div>
+            <button className={"btn"} onClick={(e) => incrementCounter()}>Increment</button>
+            <h1>Transactions Page Is Loaded {counter}</h1>
+        </div>
     )
 }
 
