1c74782fe1a1b7f9db04b63556b950f2ba1daf15
Abdulahi Hassen <abdala.baaji@gmail.com>
2026-04-20 13:20:18 +0300

    updated

--- Lab Practice/Lab 10 React Forms and State Management/Startup Code/my-finance-app/app/actions/budgetActions.js
+++ Lab Practice/Lab 10 React Forms and State Management/Startup Code/my-finance-app/app/actions/budgetActions.js
@@ -11,24 +11,6 @@ export async function createBudgetAction(prevState, formData) {
     //   Return errors if any fail
     // TODO 5d: await budgetsRepo.create(data)
     // TODO 5e: revalidatePath("/") then redirect("/budgets")
-    const budget = Object.fromEntries(formData)
-    budget.budgeted = Number(budget.budgeted);
-    budget.spent = Number(budget.spent);
-    budget.year = Number(budget.year);
-
-    // handle errors
-    const error = {};
-    if (!budget.category) error.category = "Category is required";
-    if (!budget.budgeted || budget.budgeted <= 0) error.budgeted = "Budgeted amount must be greater than 0";
-    if (!budget.month) error.month = "Month is required";
-    if (budget.year < 2020) error.year = "Year must be 2020 or later";
-
-    if (Object.keys(error).length > 0) { return error; }
-
-    await budgetsRepo.create(budget)
-
-    revalidatePath("/") //only works for server side components
-    redirect("/budgets")
 }
 
 export async function updateBudgetAction(prevState, formData) {
@@ -37,29 +19,9 @@ export async function updateBudgetAction(prevState, formData) {
     // TODO 6c: Validate (same rules as create). Return errors if any fail.
     // TODO 6d: await budgetsRepo.update(id, fields)
     // TODO 6e: revalidatePath("/") then redirect("/budgets")
-    const { id, ...fields } = Object.fromEntries(formData)
-    fields.budgeted = Number(fields.budgeted);
-    fields.spent = Number(fields.spent);
-    fields.year = Number(fields.year);
-
-    // handle errors
-    const error = {};
-    if (!fields.category) error.category = "Category is required";
-    if (!fields.budgeted || fields.budgeted <= 0) error.budgeted = "Budgeted amount must be greater than 0";
-    if (!fields.month) error.month = "Month is required";
-    if (fields.year < 2020) error.year = "Year must be 2020 or later";
-
-    if (Object.keys(error).length > 0) { return error; }
-
-    await budgetsRepo.update(id, fields)
-
-    revalidatePath("/") //only works for server side components
-    redirect("/budgets")
 }
 
 export async function deleteBudgetAction(id) {
     // TODO 7a: await budgetsRepo.delete(id)
     // TODO 7b: revalidatePath("/")
-    await budgetsRepo.delete(id)
-    revalidatePath("/")
 }
