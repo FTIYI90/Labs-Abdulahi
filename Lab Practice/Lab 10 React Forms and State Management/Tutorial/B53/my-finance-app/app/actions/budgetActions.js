a4bd674c1a348161c8f42974628d6406f3f6a28b
Abdulahi Hassen <abdala.baaji@gmail.com>
2026-04-20 13:14:28 +0300

    upto create action

--- Lab Practice/Lab 10 React Forms and State Management/Tutorial/B53/my-finance-app/app/actions/budgetActions.js
+++ Lab Practice/Lab 10 React Forms and State Management/Tutorial/B53/my-finance-app/app/actions/budgetActions.js
@@ -11,24 +11,6 @@ export async function createBudgetAction(prevState, formData) {
     //   Return errors if any fail
     // TODO 5d: await budgetsRepo.create(data)
     // TODO 5e: revalidatePath("/") then redirect("/budgets")
-    const budget = Object.fromEntries(formData)
-    budget.budgeted = Number(budget.budgeted);
-    budget.spent = Number(budget.spent);
-    budget.year = Number(budget.year);
-
-    // handle errors
-    const error = {};
-    if (!budget.category) error.category = "Category is required";
-    if (!budget.budgeted || budget.budgeted <= 0) error.budgeted = "Budgeted amount must be greater than 0";
-    if (!budget.month) error.month = "Month is required";
-    if (budget.year < 2020) error.year = "Year must be 2020 or later";
-
-    if (Object.keys(error).length > 0) { return error; }
-
-    await budgetsRepo.create(budget)
-
-    revalidatePath("/") //only works for server side components
-    redirect("/budgets")
 }
 
 export async function updateBudgetAction(prevState, formData) {
