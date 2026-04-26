0d51dcf7aa8f6865115606374398c3c02d75d3db
Abdulahi Hassen <abdala.baaji@gmail.com>
2026-04-26 17:00:58 +0300

    b01

--- Lab Practice/Lab 11 Prisma and SQLite/Tutorials/B01/my-finance-app copy/app/budgets/form/page.jsx
+++
@@ -1,6 +0,0 @@
-import BudgetForm from "@/app/components/BudgetForm";
-
-export default function BudgetFormPage() {
-    return <BudgetForm />;
-}
-
--- Lab Practice/Lab 11 Prisma and SQLite/Tutorials/B01/my-finance-app copy/app/components/SummaryCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function SummaryCard({ title, amount, variant }) {
-    return (
-        <div className={`card card--${variant}`}>
-            <h3>{title}</h3>
-            <p className="amount">{amount.toLocaleString()} QAR</p>
-        </div>
-    );
-}
